7ea1e36365b3c117e6a695e06f6362411c0544f7
Jack Hirsch <github@ojacko.com>
2016-03-29 15:00:36 -0700

    Adjusted some jokes

--- jokes.js
+++ jokes.js
@@ -8,16 +8,15 @@ function Jokes() {
   var self = this
 
   self.get = function(fn) {
-    var numbers = new Array(9);
-    numbers[0] = 'Donald Trump'
-    numbers[1] = 'A wall on the Mexican border'
-    numbers[2] = 'Curl the ear'
-    numbers[3] = 'Geech\'s showering habits'
-    numbers[4] = 'A man walks into a bar.\n Ow.'
-    numbers[5] = 'How did the scottish farmer find the sheep in the tall grass?\n Satisfying.'
-    numbers[6] = 'What do scientists and vegetables have in common?\n Stephen Hawking'
-    numbers[7] = 'What did the Jewish pedophile say to the kids?\n Hey kids, easy on the candy!'
-    numbers[8] = 'http://poncho.is/'
+    var numbers = new Array(8);
+    numbers[0] = 'Donald Trump :flag-us:'
+    numbers[1] = 'A wall on the Mexican border :flag-mx:'
+    numbers[2] = 'Curl the ear :elephant:'
+    numbers[3] = 'Geech\'s showering habits :shower:'
+    numbers[4] = 'A man walks into a bar.\n Ow. :face_with_head_bandage:'
+    numbers[5] = 'How did the scottish farmer find the sheep in the tall grass?\n Satisfying. :sheep:'
+    numbers[6] = 'What did the Jewish pedophile say to the kids?\n Hey kids, easy on the candy!'
+    numbers[7] = 'http://poncho.is/'
 
     var index = Math.floor(Math.random() * (numbers.length + 1));
 
